84e885833d78d5a9befac643823ad6d1f66e65eb
圣翔 <shengxiang.ssx@alibaba-inc.com>
2023-08-15 11:27:48 +0800

    update rgba image read support

--- imutils2.py
+++ imutils2.py
@@ -13,7 +13,9 @@ import matplotlib.pyplot as plt
 import urllib.request
 
 
-def imread(data: typing.Union[str, pathlib.Path, bytes, Image.Image, np.ndarray]) -> np.ndarray:
+AnyImage = typing.Union[str, pathlib.Path, bytes, Image.Image, np.ndarray]
+
+def imread(data: AnyImage) -> np.ndarray:
     """ convert any data to opencv type
     Support:
     - url: https://www.baidu.com/baidu.png
@@ -28,8 +30,7 @@ def imread(data: typing.Union[str, pathlib.Path, bytes, Image.Image, np.ndarray]
         if re.match(r"https?://", data):
             url = data
             with urllib.request.urlopen(url) as url_response:
-                img_pil = Image.open(url_response)
-                return imread(img_pil)
+                return imread(url_response.read())
         elif data.startswith("data:image"):
             binary_data = base64.b64decode(data.split(",")[1])
             return imread(binary_data)
@@ -50,14 +51,18 @@ def imread(data: typing.Union[str, pathlib.Path, bytes, Image.Image, np.ndarray]
             raise ValueError("Invalid image url", url)
         if len(im.shape) == 3 and im.shape[2] == 4:
             # convert rgba to rgb
-            im = cv2.cvtColor(im, cv2.COLOR_RGBA2RGB)
+            # 创建一个白色的画布，然后把非透明的部分画上去
+            background = np.full_like(im, (255, 255, 255, 255))
+            alpha_mask = im[:,:,3] > 0
+            background[alpha_mask,:] = im[alpha_mask,:]
+            im = background[:,:,:3]
+            # im = cv2.cvtColor(im, cv2.COLOR_BGRA2BGR) # 这个转化的图像有问题，所以就先不用这个方法了
         return im
     elif isinstance(data, np.ndarray):
         return data
     else:
         raise ValueError(f"Unknown data type: {type(data)}")
-        
-        
+
 
 @typechecked
 def pil2cv(pil_image: Image.Image) -> np.ndarray:
@@ -84,15 +89,14 @@ def cv2gray(cv_img: np.ndarray) -> np.ndarray:
     return cv2.cvtColor(cv_img, cv2.COLOR_BGR2GRAY)
 
 
-def show_image(images: typing.List[np.ndarray]):
+def show_image(images: typing.List[AnyImage]):
     """ Show image in jupyter """
     if not isinstance(images, list):
         images = [images]
 
     # load image using cv2....and do processing.
-    for i, img in enumerate(images):
-        if isinstance(img, Image.Image):
-            img = pil2cv(img)
+    for i, img_data in enumerate(images):
+        img = imread(img_data)
         plt.subplot(1, len(images), i + 1)
         plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
         plt.title(str(i))
